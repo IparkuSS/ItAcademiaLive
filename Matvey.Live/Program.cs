63a45cb462cdee241bd5ccef2aab17b8b8535012
iparkuSS <iparkuSS@mail.ru>
2026-05-07 19:28:08 +0300

    matv init

--- Matvey.Live/Program.cs
+++ Matvey.Live/Program.cs
@@ -5,6 +5,16 @@
         static void Main()
         {
 
+            int age = 21;
+            Console.WriteLine(age);
+            int price = 10000;
+            Console.WriteLine(price);
+            bool Oplata = true;
+            Console.WriteLine(Oplata);
+            char FirstLetter = 'M';
+            Console.WriteLine(FirstLetter);
+
+
         }
     }
 }

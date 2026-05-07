c528a2b48ec9fbdbe8e3bc3dcd1a6a63418d2c14
Александр Скрицкий <alcoreservice@gmail.com>
2026-05-07 20:21:30 +0300

    Задача исправленная

--- Aleksandr.Live/Program.cs
+++ Aleksandr.Live/Program.cs
@@ -2,7 +2,7 @@
 
 namespace Aleksandr.Live
 {
-    
+
     class Program
     {
         static void Main()
@@ -10,16 +10,18 @@ namespace Aleksandr.Live
             Console.Write("Введите число: ");
             int a = int.Parse(Console.ReadLine());
 
-            
-                if a > 0;
-                { 
+            if (a > 0)
+            {
 
                 Console.WriteLine($"Число положительное");
-                break;
-
-                if a < 0;
-                Console.WriteLine($"Число Отридцательное");
-                break;
 
             }
-        }   
+            if (a < 0)
+            {
+                Console.WriteLine($"Число Отридцательное");
+            }
+
+
+        }
+    }
+}

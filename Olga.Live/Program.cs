33de0d20f46c610b5fc6badacfa61f91d004250e
Olga Chaiko <chaiko.olga@gmail.com>
2026-05-07 20:09:07 +0300

    add

--- Olga.Live/Program.cs
+++ Olga.Live/Program.cs
@@ -4,10 +4,27 @@
     {
         static void Main()
         {
-            int userAge = 25;
-            decimal productPrice = 99.99m;
-            bool isPaid = false;
-            char firstLetterName = 'O';
+            //int userAge = 25;
+            //decimal productPrice = 99.99m;
+            //bool isPaid = false;
+            //char firstLetterName = 'O';
+
+            Console.WriteLine("Enter number");
+            var msg = Console.ReadLine();
+            bool isNumber = int.TryParse(msg, out var value);
+            if (isNumber)
+            {
+                if (value > 0)
+                {
+                    Console.WriteLine("Number positive");
+                }
+                else { Console.WriteLine("Number negative"); }
+            }
+            else {
+                Console.WriteLine("Not number");
+            }
+
+
         }
     }
 }

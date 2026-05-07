028ffd2b8d848b92a44fd0a52c23d27f14baafff
Александр Скрицкий <alcoreservice@gmail.com>
2026-05-07 19:25:05 +0300

    Переменные

--- Aleksandr.Live/Program.cs
+++ Aleksandr.Live/Program.cs
@@ -1,14 +1,14 @@
 ﻿namespace Aleksandr.Live
 {
-    string name = "Alex";
-    int n = 2;
-    double num = 10.5;
-    bool Res = false;
+    
     class Program
     {
         static void Main()
         {
-
+            string name = "Alex";
+            int n = 2;
+            double num = 10.5;
+            bool Res = false;
         }
     }
 }

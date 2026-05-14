f1aba06c4dc56304adb5b3cbc4384cbf8e781452
Александр Скрицкий <alcoreservice@gmail.com>
2026-05-14 20:04:32 +0300

    l

--- Aleksandr.Live/Program.cs
+++ Aleksandr.Live/Program.cs
@@ -333,8 +333,7 @@
             }
             static void Main(string[] args)
             {
-                int password = 0;
-
+               
                 do
                 {
                     Console.WriteLine("Введите пароль");

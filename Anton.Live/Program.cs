72ab5457addd941693d3d931470f6b1444bb7e05
ANFID <rain,playyt@yandex.ru>
2026-05-14 20:50:19 +0300

    foreach с ASCI

--- Anton.Live/Program.cs
+++ Anton.Live/Program.cs
@@ -6,49 +6,7 @@ namespace Anton.Live
     {
         static void Main()
         {
-            //int i = 0;
 
-            //while (i < 5)
-            //{
-            //    string text = Console.ReadLine();
-            //    if (int.TryParse(text, out int number))
-            //    {
-            //        if (number > 0)
-            //            Console.WriteLine("Положительное");
-            //        else
-            //            Console.WriteLine("Отрицательное");
-            //    }
-            //    else
-            //    {
-            //        Console.WriteLine("Не число ");
-
-            //    }
-            //    i++;
-
-
-
-            //try
-            //{
-            //    const decimal discontRate = 0.2M;
-
-            //    Console.WriteLine("Введите цену товара");
-            //    decimal.TryParse(Console.ReadLine(), out decimal price);
-
-            //    Console.WriteLine("Введите цену товара");
-            //    int.TryParse(Console.ReadLine(), out int count);
-
-
-
-            //    decimal subTotal = price * count;
-            //    decimal discont = subTotal * discontRate;
-            //    decimal totalPrice = subTotal - discont;
-            //    Console.WriteLine($"Итоговая цена: {totalPrice}");
-            //}
-            //catch (Exception)
-            //{
-
-            //    Console.WriteLine("Ошибка");
-            ////}
             //Console.WriteLine("Введите номер дня недели");
             //if (!(int.TryParse(Console.ReadLine(), out int day) && day < 1 && day > 7)) { Console.WriteLine("Ошибка!"); }
             //switch (day)
@@ -93,29 +51,32 @@ namespace Anton.Live
             //        break;
 
             //    }
-            //}
-            string finishPassword = "";
-            bool isPwasswordCorrect = true;
-            while (isPwasswordCorrect)
+
+            int count = default;
+            Console.WriteLine("Write word");
+            string word = Console.ReadLine();
+
+            foreach (char item in word)
             {
-                if (finishPassword == "12345")
+                if (item >= '0' && item <= '9')
                 {
-                    Console.WriteLine("Password corected");
-                    break;
+                    throw new Exception("number");
                 }
                 else
                 {
-                    Console.WriteLine("Write element");
-                    if (char.TryParse(Console.ReadLine(), out char passwordElement))
-                    {
-                        finishPassword += passwordElement;
-                    }
-                    else
-                    {
-                        Console.WriteLine("Ошибка!");
-                    }
+                    if (item == 'б') { count++; }
                 }
-            }
+
+
+                }
+
+            Console.WriteLine(count);
+
+
+
+
+
         }
+
     }
 }

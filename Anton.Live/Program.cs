0b1cf6d7fdbfd235e6e440533d889458fb5b6eba
ANFID <rain,playyt@yandex.ru>
2026-05-14 19:19:33 +0300

    switch

--- Anton.Live/Program.cs
+++ Anton.Live/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace Anton.Live
+﻿using System.Threading.Channels;
+
+namespace Anton.Live
 {
     class Program
     {
@@ -23,32 +25,65 @@
             //    }
             //    i++;
 
-            
 
-            try
+
+            //try
+            //{
+            //    const decimal discontRate = 0.2M;
+
+            //    Console.WriteLine("Введите цену товара");
+            //    decimal.TryParse(Console.ReadLine(), out decimal price);
+
+            //    Console.WriteLine("Введите цену товара");
+            //    int.TryParse(Console.ReadLine(), out int count);
+
+
+
+            //    decimal subTotal = price * count;
+            //    decimal discont = subTotal * discontRate;
+            //    decimal totalPrice = subTotal - discont;
+            //    Console.WriteLine($"Итоговая цена: {totalPrice}");
+            //}
+            //catch (Exception)
+            //{
+
+            //    Console.WriteLine("Ошибка");
+            //}
+            Console.WriteLine("Введите номер дня недели");
+            if (!(int.TryParse(Console.ReadLine(), out int day) && day < 1 && day > 7)) { Console.WriteLine("Ошибка!"); }
+            switch (day)
             {
-                const decimal discontRate = 0.2M;
+                case (1):
+                    Console.WriteLine("Сегодня понедельник");
+                    break;
+                case (2):
+                    Console.WriteLine("Сегодня вторник");
+                    break;
+                case (3):
+                    Console.WriteLine("Сегодня среда");
+                    break;
+                case (4):
+                    Console.WriteLine("Сегодня четверг");
+                    break;
+                case (5):
+                    Console.WriteLine("Сегодня пятница");
+                    break;
+                case (6):
+                    Console.WriteLine("Сегодня суббота");
+                    break;
+                case (7):
+                    Console.WriteLine("Сегодня воскресенье");
+                    break;
+                default:
+                    Console.WriteLine("");
+                    break;
 
-                Console.WriteLine("Введите цену товара");
-                decimal.TryParse(Console.ReadLine(), out decimal price);
 
-                Console.WriteLine("Введите цену товара");
-                int.TryParse(Console.ReadLine(), out int count);
-
-               
-
-                decimal subTotal = price * count;
-                decimal discont = subTotal * discontRate;
-                decimal totalPrice = subTotal - discont;
-                Console.WriteLine($"Итоговая цена: {totalPrice}");
             }
-            catch (Exception)
-            {
 
-                Console.WriteLine("Ошибка");
-            }
             
-            
-        }
+
+
         }
     }
+}

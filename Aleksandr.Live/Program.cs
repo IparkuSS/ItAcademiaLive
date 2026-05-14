929c6f75219d7a2ef5ad9658bd93ec9b7e105600
Александр Скрицкий <alcoreservice@gmail.com>
2026-05-14 19:14:20 +0300

    Lesson4

--- Aleksandr.Live/Program.cs
+++ Aleksandr.Live/Program.cs
@@ -283,6 +283,37 @@
                     Console.WriteLine("Заказ на границе 15000.");
                 }
             }
+            static void DayOfWeek(string[] args)
+            {
+                Console.WriteLine("Введите номер дня недели:");
+                int day = int.Parse(Console.ReadLine());
+
+                switch (day)
+                {
+                    case 1:
+                        Console.WriteLine("Понедельник.");
+                        break;
+                    case 2:
+                        Console.WriteLine("Вторник.");
+                        break;
+                    case 3:
+                        Console.WriteLine("Среда.");
+                        break;
+                    case 4:
+                        Console.WriteLine("Четверг.");
+                        break;
+                    case 5:
+                        Console.WriteLine("Пятница.");
+                        break;
+                    case 6:
+                        Console.WriteLine("Суббота.");
+                        break;
+                    case 7:
+                        Console.WriteLine("Воскресенье.");
+                        break;
+                }
+            }
+
         }
     }
 }

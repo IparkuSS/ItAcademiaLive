05a2c5e42fa5c434d16272521b72b021172e6942
ANFID <rain,playyt@yandex.ru>
2026-05-19 21:29:43 +0300

    Сложение 2 чисел в классах

---
+++ Anton.Live/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Anton.Live
+{
+    public class Calculator
+    {
+        public Calculator(double num1, double num2)
+        {
+            this.Num1 = num1;
+            this.Num2 = num2;
+        }
+
+        public double Num1 { get; }
+        public double Num2 { get; }
+
+        public double SumNum()
+        {
+            return Num1 + Num2;
+        }
+
+    }
+}

202b3fdfa75f35f161af94d8f62a889a348d2a66
Olga Chaiko <chaiko.olga@gmail.com>
2026-05-19 21:33:36 +0300

    udp

--- Olga.Live/Program.cs
+++ Olga.Live/Program.cs
@@ -4,47 +4,8 @@
     {
         static void Main()
         {
-
-            //int userAge = 25;
-            //decimal productPrice = 99.99m;
-            //bool isPaid = false;
-            //char firstLetterName = 'O';
-
-            //Console.WriteLine("Enter number");
-            //var msg = Console.ReadLine();
-            //bool isNumber = int.TryParse(msg, out var value);
-            //if (isNumber==true)
-            //{
-            //    if (value > 0)
-            //    {
-            //        Console.WriteLine("Number positive");
-            //    }
-            //    else { Console.WriteLine("Number negative"); }
-            //}
-            //else {
-            //    Console.WriteLine("Not number");
-            //}
-
-            decimal productPrice;
-            int productCount;
-            const int discountPercent = 15;
-            decimal totalPrice;
-
-            Console.WriteLine("Enter product price");
-            string msg = Console.ReadLine();
-            productPrice = decimal.Parse(msg);
-
-            Console.WriteLine("Enter product count");
-            msg = Console.ReadLine();
-            productCount = int.Parse(msg);
-
-            var subtotal = productPrice * productCount;
-            var discount = subtotal * discountPercent / 100;
-            var total = subtotal - discount;
-
-            Console.WriteLine("discount= " + discount + " total= " + total);
-
-
+            Numbers numbers = new Numbers(3, 1);
+            Console.WriteLine(numbers.GetSum());
         }
     }
 }

33a33da1463b5051e2291106a312241751a087af
Olga Chaiko <chaiko.olga@gmail.com>
2026-05-21 20:39:46 +0300

    add order

---
+++ Olga.Live/OrderTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Olga.Live
+{
+    class Order
+    {
+        public int Id { get; set; }
+        public string CustomerName { get; set; }
+        public decimal TotalPrice { get; set; }
+        public DateTime CreatedAt { get; private set; }
+        public bool IsPaid { get; private set; }
+
+    }
+}

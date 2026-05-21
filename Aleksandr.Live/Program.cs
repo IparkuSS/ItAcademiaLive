d4cece43ab376d12ef23baee91c81190379e1ee5
AlexSkrytski <alcoreservice@gmail.com>
2026-05-21 19:55:43 +0300

    Enum.

--- Aleksandr.Live/Program.cs
+++ Aleksandr.Live/Program.cs
@@ -2,10 +2,32 @@
 {
     class Program
     {
-        static void Main()
+        void PrintDayTime(DayTime dayTime)
         {
-            Console.WriteLine("Hello world");
 
+            switch (dayTime)
+            {
+                case DayTime.Morning:
+                    Console.WriteLine("Доброе утро");
+                    break;
+                case DayTime.Afternoon:
+                    Console.WriteLine("Добрый день");
+                    break;
+                case DayTime.Evening:
+                    Console.WriteLine("Добрый вечер");
+                    break;
+                case DayTime.Night:
+                    Console.WriteLine("Доброй ночи");
+                    break;
+            }
         }
+        enum DayTime
+        {
+            Morning,
+            Afternoon,
+            Evening,
+            Night
+        }
+
     }
 }

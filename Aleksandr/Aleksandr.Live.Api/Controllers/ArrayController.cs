dfcf560907262c7c9d68bc57060e22b5e5fa47cf
AlexSkrytski <alcoreservice@gmail.com>
2026-05-26 20:51:30 +0300

    Rev2

--- Aleksandr/Aleksandr.Live.Api/Controllers/ArrayController.cs
+++ Aleksandr/Aleksandr.Live.Api/Controllers/ArrayController.cs
@@ -1,20 +1,32 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.DataProtection.Repositories;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Aleksandr.Live.Api.Controllers
+
+
 {
     [Route("api/[controller]")]
     //[Route("api/[ArrayController]")] //same as upper expression
+    //http://localhost:5023/api/Array/analyze
     [ApiController]
     public class ArrayController : ControllerBase
     {
         [HttpPost("analyze")]
         public ActionResult<string> Analyze(int[] requestIds)
         {
-            if (requestIds.Length == 0)
+            try
             {
-                return BadRequest("empty");
+                if (requestIds.Length == 0)
+                {
 
+                    return BadRequest("empty");
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest($"Error - {ex.Message}");
             }
 
             int min = requestIds.Min();

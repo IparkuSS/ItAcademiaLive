010dea93f10053d38c84f102c7157595edf4ec71
AlexSkrytski <alcoreservice@gmail.com>
2026-05-26 20:43:00 +0300

    ClassWork.

---
+++ Aleksandr/Aleksandr.Live.Api/Controllers/ArrayController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Aleksandr.Live.Api.Controllers
+{
+    [Route("api/[controller]")]
+    //[Route("api/[ArrayController]")] //same as upper expression
+    [ApiController]
+    public class ArrayController : ControllerBase
+    {
+        [HttpPost("analyze")]
+        public ActionResult<string> Analyze(int[] requestIds)
+        {
+            if (requestIds.Length == 0)
+            {
+                return BadRequest("empty");
+
+            }
+
+            int min = requestIds.Min();
+
+            return Ok($"Min item - {min}");
+        }
+
+    }
+}

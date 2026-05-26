3b7337f7e595ca3b4bf394dd35172d17c289a868
Olga Chaiko <chaiko.olga@gmail.com>
2026-05-26 20:48:41 +0300

    add

---
+++ Olga/Olga.Live.Api/Controllers/ArrayController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Olga.Live.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArrayController : ControllerBase
+    {
+
+        [HttpPost("analyze")]
+        public ActionResult<string> Analyze(int[] requestIds)
+        {
+            int min = requestIds[0];
+
+            foreach (int id in requestIds)
+            {
+                if (id < min)
+                {
+                    min = id;
+                }
+            }
+            Console.WriteLine("Min element: " + min);
+        }
+    }
+    }
+

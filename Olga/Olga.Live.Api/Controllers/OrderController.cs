ebd1fcf98e7173c708432b3c62dd9fb237943fd8
Olga Chaiko <chaiko.olga@gmail.com>
2026-06-02 20:34:49 +0300

    add changes

--- Olga/Olga.Live.Api/Controllers/OrderController.cs
+++ Olga/Olga.Live.Api/Controllers/OrderController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Text.RegularExpressions;
 
 
 namespace Olga.Live.Api.Controllers
@@ -11,6 +12,13 @@ namespace Olga.Live.Api.Controllers
         [HttpPost("createOrder")]
         public IActionResult CreateOrder([FromBody] OrderRequest request)
         {
+            string emailRegex = @"^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$";
+
+            if (!Regex.IsMatch(request.Email, emailRegex))
+            {
+                return BadRequest("Invalid email format.");
+            }
+
             var order = new NewOrder
             {
                 OrderId = Random.Shared.Next(1, 10000),

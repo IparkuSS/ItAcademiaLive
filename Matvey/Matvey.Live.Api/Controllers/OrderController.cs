09b84d8967c35457e027afa453765549150a6eff
Shiga40 <shigamot@gmail.com>
2026-06-02 19:43:55 +0300

    change

---
+++ Matvey/Matvey.Live.Api/Controllers/OrderController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Matvey.Live;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Matvey.Live.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class OrderController : ControllerBase
+    {
+        [HttpPost("create")]
+        public IActionResult CreateOrder(OrderRequest request)
+        {
+            
+            var order = new Order
+            {
+                Id = new Random().Next(1, 10000), 
+                SecondName = "DefaultSecondName", 
+                Status = OrderStatus.Open, 
+                Name = request.Name,
+                Email = request.Email
+            };
+
+            return Ok(order.ToString());
+        }
+    }
+}
---
+++ Matvey/Matvey.Live/OrderRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Matvey.Live
+{
+    public record OrderRequest
+    {
+        public string? Name { get; set; }
+        public required string Email { get; set; }
+    }
+}

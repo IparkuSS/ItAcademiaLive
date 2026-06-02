c3be63ad3c8e7080612b91c7cc24b2cc67aea5e0
AlexSkrytski <alcoreservice@gmail.com>
2026-06-02 19:59:00 +0300

    Fix

--- Aleksandr/Aleksandr.Live.Api/Controllers/OrderRequest.cs
+++ Aleksandr/Aleksandr.Live.Api/Controllers/OrderRequest.cs
@@ -16,7 +16,7 @@ namespace Aleksandr.Live.Api.Controllers
             Email = email;
             IsClosed = isClosed;
         }
-        public override string ToString()
+        public string ToString()
         {
             return IsClosed
                 ? $"Order ID: {Id}"

e5c253523d434d1141a43aea47e1de01adbcfa55
AlexSkrytski <alcoreservice@gmail.com>
2026-06-04 13:26:51 +0300

    Added static List.

--- Aleksandr/Aleksandr.Live.Api/Controllers/OrderRequest.cs
+++ Aleksandr/Aleksandr.Live.Api/Controllers/OrderRequest.cs
@@ -1,23 +1,39 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.ComponentModel.DataAnnotations;
+﻿using Aleksandr.Live.Api.Services;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics.Eventing.Reader;
+using System.Text.RegularExpressions;
 
 namespace Aleksandr.Live.Api.Controllers
 {
-    public record OrderRequest(string Name, string Email);
+    //public record OrderRequest(int Id, string Name, string Email);
     public class Order
     {
         public int Id { get; set; }
         public string Name { get; set; }
 
-        private readonly string _email;
-
+        private string _email;
         public string Email
         {
-            get;
+            get => _email;
 
-            init => _email = IsValidEmail(value) ? value : throw new ArgumentException("Email должен содержать символ @");
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Email не может быть пустым.");
+
+                // Выражение для проверки базового формата email
+                string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+
+                // Проверка с использованием регулярного выражения
+                if (!Regex.IsMatch(value, pattern, RegexOptions.IgnoreCase))
+                {
+                    throw new ArgumentException($"Некорректный формат email: {value}");
+                }
+
+                _email = value;
+
+            }
         }
-        private bool IsValidEmail(string email) => !string.IsNullOrWhiteSpace(email) && email.Contains("@");
         public bool IsClosed { get; set; }
         public Order(int id, string name, string email, bool isClosed)
         {
@@ -42,22 +58,75 @@ namespace Aleksandr.Live.Api.Controllers
     {
 
         [HttpPost("process")]
-        public ActionResult<string> CreateOrder([FromBody] OrderRequest request)
+        public ActionResult<string> CreateOrder([FromBody] Order request)
         {
             // Принимаем Name и Email из JSON, остальные поля вручную
             var myOrder = new Order(
-                id: 101,
+                id: request.Id,
                 name: request.Name,
                 email: request.Email,
                 isClosed: false // Статус вручную
             );
 
-            return Ok(myOrder.ToString());
+            OrderService orderService = new OrderService();
 
+            if (orderService.AddOrder(myOrder))
+            {
+                return Ok($"Odrer added: {myOrder.ToString()}");
+            }
+            else
+            {
+                return BadRequest($"Order with id {myOrder.Id} allready exist!");
+            }
+        }
+
+        [HttpGet("process")]
+        public ActionResult GetOrder()
+        {
+            OrderService orderService = new OrderService();
+
+            return Ok(orderService.GetAll());
+        }
+
+        [HttpDelete("process")]
+        public ActionResult RemoveOrder([FromBody] int id)
+        {
+            OrderService orderService = new OrderService();
+
+            var isSuccess = orderService.Delete(id);
+
+            return Ok("Remaoved!");
+        }
+
+        [HttpPut("process")]
+        public ActionResult EditOrder([FromBody] Order request)
+        {
+            OrderService order = new OrderService();
+            var myOrder = new Order(
+                id: request.Id,
+                name: request.Name,
+                email: request.Email,
+                isClosed: false // Статус вручную
+            );
+
+            if (order.Update(myOrder))
+            {
+                return Ok("Update successs!");
+            }
+            else
+            {
+                return BadRequest($"Order with id: {myOrder.Id} didn`t find.");
+            }
+            
         }
     }
 }
 
+//}
+//"id": 111,
+//"name": "Иван",                    JSON запрос
+//"email": "ivan@example.com"
+//}
 
 
 
---
+++ Aleksandr/Aleksandr.Live.Api/Services/OrderService.cs
@@ -0,0 +1,61 @@
+﻿using Aleksandr.Live.Api.Controllers;
+
+namespace Aleksandr.Live.Api.Services
+{
+
+    public class OrderService
+    {
+
+        private static readonly List<Order> _orders = new()
+        {
+            new Order (id: 2, name: "Иван Иванов", email:"ivan@gmail.com",isClosed: false),
+            new Order (3, "Петр Петров", "petr@gmail.com", true)
+        };
+
+        // Получить все заказы
+        public List<Order> GetAll() => _orders;
+
+        // Добавление
+        public bool AddOrder(Order order)
+        {
+            var isOrder = _orders.FirstOrDefault(o => o.Id == order.Id);
+
+            if (isOrder != null)
+            {
+                return false;
+            }
+            else
+            {
+                _orders.Add(order);
+
+                return true;
+            }
+        }
+
+        // Удаление
+        public bool Delete(int id)
+        {
+            var order = _orders.FirstOrDefault(o => o.Id == id);
+
+            if (order == null) return false;
+
+            _orders.Remove(order);
+
+            return true;
+        }
+
+        // Изменение
+        public bool Update(Order updatedOrder)
+        {
+            var order = _orders.FirstOrDefault(o => o.Id == updatedOrder.Id);
+
+            if (order == null) return false;
+
+            order.Id = updatedOrder.Id;
+            order.Name = updatedOrder.Name;
+            order.Email = updatedOrder.Email;
+
+            return true;
+        }
+    }
+}

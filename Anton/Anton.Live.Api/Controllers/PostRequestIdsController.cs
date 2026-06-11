a03ec5a2ac6076cdbff7e789eb5be1fad991f058
ANFID <rain,playyt@yandex.ru>
2026-06-11 20:44:23 +0300

    Класс UserAccount

---
+++ Anton/Anton.Live.Api/Controllers/PostRequestIdsController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Anton.Live.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostRequestIdsController : ControllerBase
+    {
+        private static UserAccount _account = new UserAccount();
+
+        [HttpGet]
+        public IActionResult GetBalance()
+        {
+            return Ok(new { Balance = _account.GetBalance() });
+        }
+        [HttpPost("deposit")]
+        public IActionResult Deposit([FromBody] int amount)
+        {
+            _account.Deposit(amount);
+            return Ok(new{Balance = _account.GetBalance()});
+        }
+        [HttpPost("withdraw")]
+        public IActionResult Withdraw([FromBody] int amount)
+        {
+            if (!_account.TryWithdraw(amount))
+                return BadRequest("Недостаточно средств");
+
+            return Ok(new { Balance = _account.GetBalance() });
+        }
+
+
+        public class UserAccount
+        {
+            private int balance;
+
+            public UserAccount()
+            {
+                balance = 0;
+            }
+
+            public int GetBalance()
+            {
+                return balance;
+            }
+
+            public void Deposit(int amount)
+            {
+                if (IsValidAmount(amount))
+                {
+                    balance += amount;
+                }
+            }
+
+            public bool TryWithdraw(int amount)
+            {
+                if (CanWithdraw(amount))
+                {
+                    balance -= amount;
+                    return true;
+                }
+                return false;
+            }
+
+            private bool IsValidAmount(int amount)
+            {
+                return amount > 0;
+            }
+
+            private bool CanWithdraw(int amount)
+            {
+                return IsValidAmount(amount) && balance >= amount;
+            }
+        }
+    }
+}
+

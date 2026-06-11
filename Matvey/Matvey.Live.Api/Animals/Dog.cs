1d94b200387f96c6003ef749855b9c9d1533b77e
Matvey <shigamot@gmail.com>
2026-06-11 21:12:55 +0300

    Animal

---
+++ Matvey/Matvey.Live.Api/Animals/Dog.cs
@@ -0,0 +1,17 @@
+﻿namespace Matvey.Live.Api.Animals
+{
+    public class Dog : Animal
+    {
+        public string Breed { get; set; }
+
+        public Dog(string name, string breed) : base(name)
+        {
+            Breed = breed;
+        }
+
+        public override string Speak()
+        {
+            return $"{Name} says: Woof! Woof!";
+        }
+    }
+}

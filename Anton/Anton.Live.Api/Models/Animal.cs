2f4dfc8a1e8103a2f83fc260c3a6b0b71074bf99
ANFID <rain,playyt@yandex.ru>
2026-06-11 21:28:15 +0300

    Добавил класс Animal

---
+++ Anton/Anton.Live.Api/Models/Animal.cs
@@ -0,0 +1,30 @@
+﻿namespace Anton.Live.Api.Models
+{
+    public class Animal
+    {
+        public string Name { get; set; }
+        public Animal(string name)
+        {
+            Name = name;
+        }
+        public virtual string Speak()
+        {
+            return $"Звук...";
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public string Color { get; set; }
+
+        public Cat(string Name, string color) : base(Name)
+        {
+            Color = color;
+        }
+        public override string Speak()
+        {
+            return $"{Name} цвета {Color} говорит Мяу";
+        }
+    }
+}
+

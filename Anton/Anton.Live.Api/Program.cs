1d726315848aec71a661ce127d961ff52e515ba5
ANFID <rain,playyt@yandex.ru>
2026-06-16 21:07:39 +0300

    Исправление

--- Anton/Anton.Live.Api/Program.cs
+++ Anton/Anton.Live.Api/Program.cs
@@ -17,22 +17,27 @@ using Anton.Live.Api.Models;
 using Anton.Live.Api.Services;
 
 ProductReader reader = new ProductReader();
-
-Console.WriteLine("Введите ID товара:");
-int.TryParse(Console.ReadLine(), out int searchId);
-
-Product foundProduct = reader.GetProductById(searchId);
-
-if (foundProduct != null)
-{
-    Console.WriteLine("Название: " + foundProduct.Name);
-    Console.WriteLine("Цена: " + foundProduct.Price + " руб");
-}
-else
-{
-    Console.WriteLine("Товар с таким ID не найден");
-}
+Parse();
 
 reader.AddProduct(new Product { Id = 4, Name = "Сыр", Price = 250.00m });
+Parse();
 Console.WriteLine(reader.DeleteProductById(2));
+Parse();
 
+ void Parse()
+{
+    Console.WriteLine("Введите ID товара:");
+    int.TryParse(Console.ReadLine(), out int searchId);
+
+    Product foundProduct = reader.GetProductById(searchId);
+
+    if (foundProduct != null)
+    {
+        Console.WriteLine("Название: " + foundProduct.Name);
+        Console.WriteLine("Цена: " + foundProduct.Price + " руб");
+    }
+    else
+    {
+        Console.WriteLine("Товар с таким ID не найден");
+    }
+}

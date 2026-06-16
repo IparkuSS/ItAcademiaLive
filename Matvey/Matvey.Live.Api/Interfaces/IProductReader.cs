db85faf6ff2c779570aaa5f2d88896f199f30dff
Matvey <shigamot@gmail.com>
2026-06-16 20:59:56 +0300

    Interface add and remove

---
+++ Matvey/Matvey.Live.Api/Interfaces/IProductReader.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Collections.Generic;
+using Matvey.Live.Api.Models;
+
+namespace Matvey.Live.Api.Interfaces
+{
+    public interface IProductReader
+    {
+        Product GetProductById(int id);
+        IEnumerable<Product> GetAllProducts();
+        bool ProductExists(int id);
+    }
+}

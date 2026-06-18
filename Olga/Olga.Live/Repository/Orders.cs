c6f461d56e36543de080542cd581ce70a7f0229f
Olga Chaiko <chaiko.olga@gmail.com>
2026-06-18 21:13:57 +0300

    add changes

---
+++ Olga/Olga.Live/Repository/Orders.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Olga.Live.Repository
+{
+    public class Order2
+
+    {
+        public int Id { get; set; }
+        public string Number { get; set; }
+    }
+
+    public class OrdersRepository : IRepository<Order2>
+    {
+        public Order2 GetById(int id)
+        {
+            return new Order2 { Id = id, Number = "001" };
+        }
+        public void Add(Order2 item) { }
+
+        public void Update(Order2 item) { }
+        public void Delete(int id) { }
+    }
+}
---
+++ Olga/Olga.Live/Repository/Products.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Olga.Live.Repository
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public class ProductRepository : IRepository<Product>
+    {
+        public Product GetById(int id)
+        {
+            return new Product { Id = id, Name = "Product" };
+        }
+
+        public void Add(Product item) { }
+
+        public void Update(Product item) { }
+
+        public void Delete(int id) { }
+    }
+}

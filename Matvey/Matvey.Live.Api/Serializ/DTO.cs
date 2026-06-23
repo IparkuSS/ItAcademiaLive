3d220df50facf0204abf9493fc598038966ab7f7
Matvey <shigamot@gmail.com>
2026-06-23 20:36:43 +0300

    Serializ

---
+++ Matvey/Matvey.Live.Api/Serializ/DTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Matvey.Live.Api.Serializ
+{
+    public class OrderDto
+    {
+
+        public Guid OrderId { get; set; }
+        public string CustomerName { get; set; }
+        public decimal TotalAmount { get; set; }
+        public DateTime OrderDate { get; set; }
+        public bool IsPaid { get; set; }
+    }
+}

f7c435dc027bacb01b24a649746733e0f3b5f18b
AlexSkrytski <alcoreservice@gmail.com>
2026-06-25 20:33:29 +0300

    Add endoint Sort by Id.

--- Aleksandr/Aleksandr.Live.Api/Controllers/OrdersController.cs
+++ Aleksandr/Aleksandr.Live.Api/Controllers/OrdersController.cs
@@ -1,4 +1,6 @@
 ﻿using Aleksandr.Live.Api.Domains;
+using Aleksandr.Live.Api.DTO;
+using Aleksandr.Live.Api.Services;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Aleksandr.Live.Api.Controllers
@@ -11,19 +13,47 @@ namespace Aleksandr.Live.Api.Controllers
         private readonly List<Order> _orders = new List<Order>
     {
         new Order { Id = 1, Name = "Заказ 1", Status = "Create" },
-        new Order { Id = 2, Name = "Заказ 2", Status = "Process" },
-        new Order { Id = 3, Name = "Заказ 3", Status = "Create" }
+        new Order { Id = 2, Name = "Заказ 2", Status = "Process"},
+        new Order { Id = 3, Name = "Заказ 3", Status = "Create" },
+        new Order { Id = 4, Name = "Заказ 4", Status = "Process" },
+        new Order { Id = 5, Name = "Заказ 5", Status = "Create" },
+
     };
 
         [HttpGet("created-names")]
         public ActionResult<List<string>> GetCreatedOrderNames()
         {
             List<string> createdNames = _orders
-.Where(o => o.Status == "Create")
-.Select(o => o.Name)
-.ToList();
+            .Where(o => o.Status == "Create")
+            .Select(o => o.Name)
+            .ToList();
 
             return Ok(createdNames);
+        }
+
+        [HttpPost("sorted-names-by-id")]
+        public ActionResult<List<string>> GetSortedNamesById([FromBody] SortRequest request)
+        {
+            var query = _orders.Where(o => o.Status == "Create");
+
+            if (request?.Direction?.ToLower() == "desc")
+            {
+                query = query.OrderByDescending(o => o.Id);
+            }
+            else
+            {
+                query = query.OrderBy(o => o.Id); //Дефолт (asc)
+            }
+
+            List<OrderResponseDto> result = query
+             .Select(o => new OrderResponseDto
+             {
+                 Id = o.Id,
+                 Name = o.Name
+             })
+             .ToList();
+
+            return Ok(result);
         }
     }
 }
---
+++ Aleksandr/Aleksandr.Live.Api/Services/SortRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Aleksandr.Live.Api.Services
+{
+    public class SortRequest
+    {
+        public string Direction { get; set; } = string.Empty; // "asc" или "desc"
+    }
+}

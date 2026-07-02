3285912b33af93a23528d20b0cfe8a557053f622
Shiga40 <shigamot@gmail.com>
2026-07-02 20:04:47 +0300

    Exceptions

---
+++ Matvey/Matvey.Live.Api/CustomException/Exceptions/AgeException.cs
@@ -0,0 +1,15 @@
+﻿namespace Matvey.Live.Api.CustomException.Exceptions
+{
+    public class InvalidAgeException : Exception
+    {
+        public int InvalidAge { get; }
+
+        public InvalidAgeException() : base("Возраст должен быть положительным.") { }
+
+        public InvalidAgeException(int age, string message)
+            : base($"{message} Возраст: {age}")
+        {
+            InvalidAge = age;
+        }
+    }
+}

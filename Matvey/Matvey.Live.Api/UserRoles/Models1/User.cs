c7028f0f9f93210da77ab62085aa5e358283a65a
Shiga40 <shigamot@gmail.com>
2026-07-02 20:58:46 +0300

    USerRole

---
+++ Matvey/Matvey.Live.Api/UserRoles/Models1/User.cs
@@ -0,0 +1,25 @@
+﻿using Matvey.Live.Api.UserRoles.Roles;
+
+namespace Matvey.Live.Api.UserRoles.Models1
+{
+   
+    public class User
+    {
+        public string Name { get; set; }
+        public UserRole Role { get; set; }
+
+        public User(string name, UserRole role)
+        {
+            Name = name;
+            Role = role;
+        }
+
+        public void ShowPermissions()
+        {
+            Console.WriteLine($"\nПользователь: {Name}");
+            Console.WriteLine($"Роль: {Role.GetType().Name}");
+            Console.WriteLine($"Может удалять: {(Role.CanDelete() ? "Да" : "Нет")}");
+        }
+    }
+}
+
---
+++ Matvey/Matvey.Live.Api/UserRoles/Roles/UserRole.cs
@@ -0,0 +1,11 @@
+﻿namespace Matvey.Live.Api.UserRoles.Roles
+{
+   
+    public abstract class UserRole
+    {
+        public virtual bool CanDelete()
+        {
+            return false;
+        }
+    }
+}

3ba7610cae814b5d33533b5a57db936f7c737186
ANFID <rain,playyt@yandex.ru>
2026-07-14 20:55:38 +0300

    LoadUserAndSettingsAsync

--- Anton/Anton.Live.Api/Program.cs
+++ Anton/Anton.Live.Api/Program.cs
@@ -1,7 +1,11 @@
+using Anton.Live.Api.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
+
+builder.Services.AddScoped<DataService>();
 
 var app = builder.Build();
 

e2df9ab748e2cb9d658ff35143a35adc3b0af3e9
Matvey <shigamot@gmail.com>
2026-07-16 20:33:24 +0300

    Atribute

---
+++ Matvey/Matvey.Live/Atribute.cs
@@ -0,0 +1,21 @@
+﻿namespace Matvey.Live
+{
+
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Field | AttributeTargets.Class | AttributeTargets.Struct,
+                    AllowMultiple = false,
+                    Inherited = true)]
+    public class DisplayNameAttribute : Attribute
+    {
+        public string DisplayName { get; }
+        public string Description { get; set; }
+        public int Order { get; set; }
+
+        public DisplayNameAttribute(string displayName)
+        {
+            if (string.IsNullOrWhiteSpace(displayName))
+                throw new ArgumentException("DisplayName не может быть null, пустым или состоять только из пробелов", nameof(displayName));
+
+            DisplayName = displayName;
+        }
+    }
+}
